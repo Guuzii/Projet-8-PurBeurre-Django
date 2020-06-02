8f576fd23cd20618113c8b764db16e6e12f7e981
Erwan <morioe2809@gmail.com>
2020-06-02 17:12:38 +0200

    update settings pour heroku

--- pureBeurreOC/pureBeurreOC/settings.py
+++ pureBeurreOC/pureBeurreOC/settings.py
@@ -131,13 +131,12 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(BASE_DIR, 'products\\static')
+    os.path.join(BASE_DIR, 'products/static')
 )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
 # Open Food Facts API requests variables
